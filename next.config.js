d72d52f35776369ce03c36780240f4ae9591f7b1
Deandre Moore <deandremoore@Dres.local>
2025-01-04 13:19:55 -0800

    update

--- next.config.js
+++ next.config.js
@@ -4,16 +4,13 @@ const nextConfig = {
   swcMinify: true,
   compress: true,
   images: {
-    unoptimized: true,  // Required for static export
     domains: ['kindly.sh'],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  output: 'export',
+  output: 'standalone',
   poweredByHeader: false,
-  trailingSlash: true,
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
 }
 
 module.exports = nextConfig
